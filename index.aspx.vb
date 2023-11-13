a9d86ca1b3d91bf382daaeed15d410150a45bbd7
nantawat <112058120+udosmak99@users.noreply.github.com>
2023-11-13 17:35:28 +0700

    Add files via upload

---
+++ index.aspx.vb
@@ -0,0 +1,8 @@
+
+Partial Class index
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Response.Redirect("./pages/pageLogin.aspx")
+    End Sub
+End Class
